b448f4bb1a1cecedcbee249b451cc768b8cb99f7
Amjad Khalaf <92156267+Amjad-Khalaf@users.noreply.github.com>
2024-10-21 10:47:36 +0100

    Create file.h

---
+++ file.h
@@ -0,0 +1,11 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function to read a file and print its contents
+void read_file(const char *filename);
+
+#endif // FILE_H
